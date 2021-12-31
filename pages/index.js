3bf1880a33a812355f52ef45aeab8859c1445e33
fangchaoqun <fang.chaoqun@nissei-china.cn>
2021-12-31 17:42:56 +0800

    feat: 首页背景图片

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
+import { useRef, useEffect } from "react";
 import LayoutCom from "@/components/LayoutCom";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
+  const wrapperRef = useRef(null);
+  const h1Ref = useRef(null);
+
+  const handleChangeBgImg = () => {
+    console.log(wrapperRef);
+  };
+
+  useEffect(() => {
+    h1Ref.current?.addEventListener("click", handleChangeBgImg);
+
+    return () => {
+      h1Ref.current?.removeEventListener("click", handleChangeBgImg);
+    };
+  }, []);
+
   return (
     <LayoutCom home>
-      <div className={styles.container}>
-        <h1 style={{ color: "red" }}>container</h1>
+      <div className={styles.wrapper} ref={wrapperRef}>
+        <div className={styles.container}>
+          <h1 className={styles.header} ref={h1Ref}>
+            个人博客
+          </h1>
+        </div>
       </div>
     </LayoutCom>
   );

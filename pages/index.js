3c5be8c730af75ce583d5d4fde0076a301197fdc
fangchaoqun <fang.chaoqun@nissei-china.cn>
2022-01-04 14:35:59 +0800

    feat: 首页背景图片animation

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,36 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useCallback } from "react";
 import LayoutCom from "@/components/LayoutCom";
 import styles from "@/styles/Home.module.css";
+
+// https://nextjs.org/docs/basic-features/image-optimization#local-images
+const allBgList = require.context("../public/images", false, /\.png$/);
 
 export default function Home() {
   const wrapperRef = useRef(null);
   const h1Ref = useRef(null);
+  const bgNumRef = useRef(1);
+  const bgTotalNum = allBgList.keys().length / 2;
 
-  const handleChangeBgImg = () => {
-    console.log(wrapperRef);
-  };
+  const handleChangeBgImg = useCallback(() => {
+    console.log("click!!!", wrapperRef.current.className);
+    bgNumRef.current += 1;
+
+    if (bgNumRef.current > bgTotalNum) bgNumRef.current = 1;
+
+    document.body.style.setProperty(
+      "--home-background",
+      `url(/images/test${bgNumRef.current}.png)`
+    );
+  }, [bgTotalNum]);
 
   useEffect(() => {
-    h1Ref.current?.addEventListener("click", handleChangeBgImg);
+    h1Ref.current?.addEventListener("click", handleChangeBgImg, false);
 
     return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
       h1Ref.current?.removeEventListener("click", handleChangeBgImg);
     };
-  }, []);
+  }, [handleChangeBgImg]);
 
   return (
     <LayoutCom home>

c6ad2a3ba069d40053e5a078a7737340fe533eef
fangchaoqun <fang.chaoqun@nissei-china.cn>
2022-01-06 16:31:07 +0800

    perf: optimize home img

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,23 @@ import styles from "@/styles/Home.module.css";
 // https://nextjs.org/docs/basic-features/image-optimization#local-images
 const allBgList = require.context("../public/images", false, /\.png$/);
 
+// https://nextjs.org/docs/api-reference/next/image#blurdataurl
+const keyStr =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+const triplet = (e1, e2, e3) =>
+  keyStr.charAt(e1 >> 2) +
+  keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+  keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+  keyStr.charAt(e3 & 63);
+
+const rgbDataURL = (r, g, b) =>
+  `data:image/gif;base64,R0lGODlhAQABAPAA${
+    triplet(0, r, g) + triplet(b, 255, 255)
+  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+
 const Home = () => {
   const [bgImg, setBgImg] = useState(() => "/images/test1.png");
-  const wrapperRef = useRef(null);
   const h1Ref = useRef(null);
   const bgNumRef = useRef(1);
   const bgTotalNum = allBgList.keys().length / 2;
@@ -44,7 +58,7 @@ const Home = () => {
 
   return (
     <LayoutCom home>
-      <div className={styles.wrapper} ref={wrapperRef}>
+      <div className={styles.wrapper}>
         <Image
           id="bgImg"
           className={styles.homeBgImg}
@@ -52,6 +66,9 @@ const Home = () => {
           layout="fill"
           objectFit="cover"
           objectPosition="center"
+          quality={65}
+          placeholder="blur"
+          blurDataURL={rgbDataURL(2, 129, 210)}
           alt="img"
         />
         <div className={styles.container}>
